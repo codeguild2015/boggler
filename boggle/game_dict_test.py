0e4978d7d2de0b71964ad25e7d9e6eca7f94b782
saashimi <saavedra.kevin@gmail.com>
2015-10-07 09:15:52 -0700

    Wednesday morning, semi-working test. Problems with global variable words.

--- boggle/game_dict_test.py
+++ boggle/game_dict_test.py
@@ -7,11 +7,10 @@ read(file, 3)
 
 """TEST 0"""
 assert words == []
-print("Passed test 0! ", words)
+print("Passed test 0! words =", words)
 
 """TEARDOWN 0"""
-del words 
-
+del words
 from game_dict import *
 
 """SETUP 1"""
@@ -20,20 +19,31 @@ read(file, 3)
 
 """TEST 1"""
 assert words == ['hello']
-print("Passed test 1! ", words)
+print("Passed test 1! words =", words)
 
 """TEARDOWN 1"""
-del words 
-
+del words
 from game_dict import *
 
 """SETUP 2"""
-file = 'in-line'
+file = ['in-line']
 read(file, 3)
 
 """TEST 2"""
-assert words == []
-print("Passed test 2! ", words)
+assert words == ['hello']
+print("Passed test 2! words =", words)
 
 """TEARDOWN 2"""
+del words 
+from game_dict import *
+
+"""SETUP 3"""
+file = ['final']
+read(file, 3)
+
+"""TEST 3"""
+assert words == ['hello', 'final']
+print("Passed test 2! words =", words)
+
+"""TEARDOWN 3"""
 del words 
