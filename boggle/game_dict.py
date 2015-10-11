2ea6dd838ff25ac14ac137a31eadbd86b242c2ad
Pjmcnally <Patrick.J.McNally@gmail.com>
2015-10-11 00:52:40 -0700

    implement single loop binary search.  add asserts for testing

--- boggle/game_dict.py
+++ boggle/game_dict.py
@@ -41,59 +41,62 @@ def read(file1, min_length=3 ):
 def search(str1, lst):
     max = len(lst)
     min = 0
-    if max <= 3:
-        if str1 in lst:
-            return word
+    if lst[max//2] == str1 or max <= 3:
+        if str1 == lst[max//2]\
+        or str1 == lst[min]\
+        or str1 == lst[max-1]:
+            return WORD
+        elif lst[min].startswith(str1)\
+        or lst[max//2].startswith(str1)\
+        or lst[max-1].startswith(str1):      
+            return PREFIX
         else:
-            return prefix
-    if lst[max//2].startswith(str1):
-        return search(str1, lst[max//2:])
-    if max//2 == 0:
-        return NO_MATCH
+            return NO_MATCH
     else:
-        if str1 > lst[max//2]:
-            return search(str1, lst[max//2:])
+        if str1 < lst[max//2]:
+            return search(str1, lst[:max//2 + 1]) # +1 to be inclusive
         else:
-            return search(str1, lst[:max//2])
-    # def is_word(str1, lst):
-    #     max = len(lst)
-    #     min = 0
-    #     if str1 == lst[max//2]:
-    #         return WORD
-    #     if max//2 == 0:
-    #         return NO_MATCH
-    #     else:
-    #         if str1 > lst[max//2]:
-    #             return is_word(str1, lst[max//2:])
-    #         else:
-    #             return is_word(str1, lst[:max//2])
+            return search(str1, lst[max//2:])
+       
+# def search_double_loop(str1, lst):      
+#     def is_word(str1, lst):
+#         max = len(lst)
+#         min = 0
+#         if str1 == lst[max//2]:
+#             return WORD
+#         if max//2 == 0:
+#             return NO_MATCH
+#         else:
+#             if str1 > lst[max//2]:
+#                 return is_word(str1, lst[max//2:])
+#             else:
+#                 return is_word(str1, lst[:max//2])
 
 
-    # def is_prefix(str1, lst):
-    #     max = len(lst)
-    #     min = 0
-    #     if lst[max//2].startswith(str1):
-    #         return PREFIX
-    #     if max//2 == 0:
-    #         return NO_MATCH
-    #     else:
-    #         if str1 > lst[max//2]:
-    #             return is_prefix(str1, lst[max//2:])
-    #         else:
-    #             return is_prefix(str1, lst[:max//2])
-
+#     def is_prefix(str1, lst):
+#         max = len(lst)
+#         min = 0
+#         if lst[max//2].startswith(str1):
+#             return PREFIX
+#         if max//2 == 0:
+#             return NO_MATCH
+#         else:
+#             if str1 > lst[max//2]:
+#                 return is_prefix(str1, lst[max//2:])
+#             else:
+#                 return is_prefix(str1, lst[:max//2])
     
-    # val = is_word(str1, lst)
-    # if val == WORD:
-    #     return WORD
-    # if val == NO_MATCH:
-    #     if is_prefix(str1, lst) == PREFIX:
-    #         return PREFIX
-    #     else: 
-    #         return NO_MATCH
+#     val = is_word(str1, lst)
+#     if val == WORD:
+#         return WORD
+#     if val == NO_MATCH:
+#         if is_prefix(str1, lst) == PREFIX:
+#             return PREFIX
+#         else: 
+#             return NO_MATCH
 
 
-# def search(str1, words ):
+# def search_linear(str1, words ):
 #     """Search for a prefix string in the dictionary.
 #     Args:
 #         str:  A string to look for in the dictionary
@@ -111,11 +114,11 @@ def search(str1, lst):
 #                 return PREFIX
 #         return NO_MATCH
 
-    # FIXME: I suggest using a linear search first, checking for exact matches
-    # with == and then for partial matches with the "startswith" function, e.g.,
-    # words[i].startswith(prefix). 
-    # Once you get the whole program working, you can make it much, much faster
-    # using a binary search (which we will discuss in class). 
+#     # FIXME: I suggest using a linear search first, checking for exact matches
+#     # with == and then for partial matches with the "startswith" function, e.g.,
+#     # words[i].startswith(prefix). 
+#     # Once you get the whole program working, you can make it much, much faster
+#     # using a binary search (which we will discuss in class). 
     
     
 ######################################################
@@ -140,33 +143,40 @@ if __name__ == "__main__":
     # This code executes only if we execute game_dict.py by itself,
     # not if we import it into boggler.py
     from test_harness import testEQ
-    read(open("shortdict.txt"))
+    word_dict = read(open("shortdict.txt"))
     # shortdict contains "alpha", "beta","delta", "gamma", "omega"
-    testEQ("First word in dictionary (alpha)", search("alpha"), WORD)
-    testEQ("Last word in dictionary (omega)", search("omega"), WORD)
-    testEQ("Within dictionary (beta)", search("beta"), WORD)
-    testEQ("Within dictionary (delta)", search("delta"), WORD)
-    testEQ("Within dictionary (gamma)", search("gamma"), WORD)
-    testEQ("Prefix of first word (al)", search("al"), PREFIX)
-    testEQ("Prefix of last word (om)", search("om"), PREFIX)
-    testEQ("Prefix of interior word (bet)", search("bet"),PREFIX)
-    testEQ("Prefix of interior word (gam)", search("gam"),PREFIX)
-    testEQ("Prefix of interior word (del)", search("del"),PREFIX)
-    testEQ("Before any word (aardvark)", search("aardvark"), NO_MATCH)
-    testEQ("After all words (zephyr)", search("zephyr"), NO_MATCH)
-    testEQ("Interior non-word (axe)", search("axe"), NO_MATCH)
-    testEQ("Interior non-word (carrot)", search("carrot"), NO_MATCH)
+    testEQ("First word in dictionary (alpha)", search("alpha", word_dict), WORD)
+    testEQ("Last word in dictionary (omega)", search("omega", word_dict), WORD)
+    testEQ("Within dictionary (beta)", search("beta", word_dict), WORD)
+    testEQ("Within dictionary (delta)", search("delta", word_dict), WORD)
+    testEQ("Within dictionary (gamma)", search("gamma", word_dict), WORD)
+    testEQ("Prefix of first word (al)", search("al", word_dict), PREFIX)
+    testEQ("Prefix of last word (om)", search("om", word_dict), PREFIX)
+    testEQ("Prefix of interior word (bet)", search("bet", word_dict),PREFIX)
+    testEQ("Prefix of interior word (gam)", search("gam", word_dict),PREFIX)
+    testEQ("Prefix of interior word (del)", search("del", word_dict),PREFIX)
+    testEQ("Before any word (aardvark)", search("aardvark", word_dict), NO_MATCH)
+    testEQ("After all words (zephyr)", search("zephyr", word_dict), NO_MATCH)
+    testEQ("Interior non-word (axe)", search("axe", word_dict), NO_MATCH)
+    testEQ("Interior non-word (carrot)", search("carrot", word_dict), NO_MATCH)
     testEQ("Interior non-word (hagiography)",
-        search("hagiography"), NO_MATCH)
+        search("hagiography", word_dict), NO_MATCH)
     # Try again with only words of length at least 5
     # Now beta should be absent
-    read(open("shortdict.txt"), min_length=5)
-    print("New dictionary: ", dict)
-    testEQ("First word in dictionary (alpha)", search("alpha"), WORD)
-    testEQ("Last word in dictionary (omega)", search("omega"), WORD)
-    testEQ("Short word omitted (beta)", search("beta"), NO_MATCH)
-    read(open("dict.txt"))  # Long dictioanry
-    testEQ("Can I find farm in long dictonary?", search("farm"), WORD)
-    testEQ("Can I find bead in long dictionary?", search("bead"), WORD)
+    word_dict = read(open("shortdict.txt"), min_length=5)
+    testEQ("First word in dictionary (alpha)", search("alpha", word_dict), WORD)
+    testEQ("Last word in dictionary (omega)", search("omega", word_dict), WORD)
+    testEQ("Short word omitted (beta)", search("beta", word_dict), NO_MATCH)
+    word_dict = read(open("dict.txt"))  # Long dictioanry
+    testEQ("Can I find farm in long dictonary?", search("farm", word_dict), WORD)
+    testEQ("Can I find bead in long dictionary?", search("bead", word_dict), WORD) 
+    #my asserts to test binary search algorithm
+    word_dict = read(open("sowpods.txt"), min_length=3)
+    testEQ("Words in sowpods.txt (al)", search("al", word_dict), PREFIX)
+    testEQ("Words in sowpods.txt (alp)", search("alp", word_dict), WORD) 
+    testEQ("Words in sowpods.txt (alph)", search("alph", word_dict), PREFIX) 
+    testEQ("Words in sowpods.txt (alpha)", search("alpha", word_dict), WORD)
+    testEQ("Words in sowpods.txt (alphb)", search("alphb", word_dict), NO_MATCH)
+
     
     
