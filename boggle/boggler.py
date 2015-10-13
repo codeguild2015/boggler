a205c803742fbc14e74e6e6835e0374e4028cb11
ransomv <rvarley2001@yahoo.com>
2015-10-12 20:09:04 -0700

    working version of find_words, score and rem_dups function.  still needs to sort output alphabetically

--- boggle/boggler.py
+++ boggle/boggler.py
@@ -11,6 +11,7 @@ Usage:  python3 boggler.py  "board" dict.txt
 from boggle_board import BoggleBoard   
 import argparse   # Command line processing
 import game_dict  # Dictionary of legal game words
+from collections import OrderedDict
 
 def main():
     """
@@ -24,20 +25,63 @@ def main():
         order, without duplicates, one word per line)
     """
     dict_file, board_text = getargs()
-    game_dict.read( dict_file )
-    
+
+
     board = BoggleBoard(board_text)
-    
+    prefix = "" 
+    #prefix = board.get_char(0, 0)
     results = [ ] 
     #game_dict = game_dict.read( dict_file ) #KS: added this to initialize
-    init_find_words = find_words(board, 0, 0, board.get_char(0,0), results)
-    
+    game_dict.read( dict_file )
+
+    for i in range(4):
+        for j in range(4):
+            print("calling find_words from main with row, col - ", i, j)
+            # board.unmark_all()
+            find_words(board, i, j, prefix, results)
+    print("results at end of main:  ", results)
+    uniq_res = rem_dups(results)
+    print("Unique results at end of main are:  ", uniq_res)
+    score(uniq_res)
+
+
     # FIXME: 
     #    Search for words starting from each position on the board. 
     #    Remove duplicates from results, and sort them alphabetically.
     #        (Write a separate function for dedu
     #    Print each word and its score
     #    Print total score
+
+def rem_dups(results):
+    return list(OrderedDict.fromkeys(results))
+
+
+def score(results):
+    """
+    Compute the Boggle score for a word, based on the scoring table
+    at http://en.wikipedia.org/wiki/Boggle. 
+    #FIXME: finish writing this docstring
+     """
+    print("In score function")
+    score_dct = {3 : 1, 4: 1, 5 : 2, 6 : 3, 7 : 5,}
+    final_score = []
+    for word in results:
+        print("Word in score is {}.  Length of word is {} .".format(word, len(word)))
+        if len(word) >= 8:
+            final_score.append(11)
+        else:
+            final_score.append(score_dct[len(word)])
+    for i in range(len(final_score)):
+        print (results[i], final_score[i])
+            
+    final_score = sum(final_score)
+    print("final score is: ", final_score)
+    
+    # return(final_score)
+                          
+#    assert score(["alp", "alpha", "gal", "gamma", "hap", "lag", "lam", 
+#        "mag", "max"]) = 11 
+
 
 
 def getargs():
@@ -65,7 +109,49 @@ def getargs():
     return dictfile, text
 
 
-        
+def find_words(board, row, col, prefix, results):
+    """Find all words starting with prefix that
+    can be completed from row,col of board.
+    Args:
+        row:  row of position to continue from (need not be on board)
+        col:  col of position to continue from (need not be on board)
+        prefix: A string.  looking for words that start with this prefix
+        results: list of words found so far
+    Returns: nothing
+        (side effect is filling results list)
+    Effects:
+        inserts found words (not necessarily unique) into results
+    """
+
+    """Extracting content
+    for item in BoggleBoard.content:
+        row = BoggleBoard.content[item]
+        for column in row: 
+            col = row[column]
+    """
+
+    board.mark_taken(row, col)
+
+    for i in range(row - 1, row + 2):
+        for j in range(col - 1, col + 2):
+            if board.available(i, j) is True:
+                print('get_char looking for location: ', row + i, col + j)
+                prefix += board.get_char(i, j)
+                is_word = game_dict.search(prefix)
+                if is_word == 1:  # prefix is a word
+                    results.append(prefix)
+                    find_words(board, i, j, prefix, results)
+                elif is_word == 2:  # prefix is a prefix but not also a word
+                    find_words(board, i, j, prefix, results)  # need to fix args
+                if board.get_char(i, j) == 'qu':
+                    prefix = prefix[:-2]
+                else:
+                    prefix = prefix[:-1]
+    board.unmark_taken(row, col)
+    print("Reults at end of nested for: ", results)
+    return results
+
+
 def find_words_1(board, row, col, prefix, results):
     """Find all words starting with prefix that
     can be completed from row,col of board.
@@ -86,148 +172,52 @@ def find_words_1(board, row, col, prefix, results):
         for column in row: 
             col = row[column]
     """
-    #cur_word = board.content[row][col] # !! KS this isn't called again
-
-    if prefix == None:
-        return "No prefixes!"
-
-    print(row, col)
-
-    #if board.available(row, col) is False:
-    #    row = row + 1
-    #    col = col + 1
-
-    if board.available(row, col) is True:
-        #dictionary = ["alpha"]
-        prefix = []
-        row_offset = [-1, 0, 1]
-        column_offset = [-1, 0, 1]
-
-        strings_ = ""
-        strings_ += board.content[row][col]
-        board.mark_taken(row, col)
-        print("origin tile at start of find_words: ", strings_)
-        #counter = 1
-        
-        for i in row_offset:
-            next_row = row + i
-            for j in column_offset:
-                
-                #counter += 1
-                print("i and j at outer for is:  ", i, j)
-                # next_row = row + i
-                next_col = col + j  
-                
-
-                if (row >= 0 and next_col >= 0) and (row <= 3 and 
-                    next_col <= 3):
-                    strings_ = strings_ + board.content[next_row][next_col]
-                    print('value of strings_ after appending it: ', strings_)
-                    if board.available(row, col) is True: 
-                        prefix.append(strings_)
-                        #board.mark_taken(next_row, next_col)
-                                              
-                    if i == 1 and j == 1:
-
-                        find_words(board, row, col + 1, 
-                            board.content[row][next_col], results)
-                        
-
-
-                        #row = next_row - row
-                        #col = next_col - col
-
-
-                        #print("next_row, col =", next_row, next_col)
-                        #print("row, col =", row, col)
-
-            #find_words(board, row, col, board.content[row][col], results)
-        
-        #find_words(board, next_row, next_col, board.content[next_row][next_col], 
-        #    results)
-
-        print(prefix)
-
-
-        #for words in prefix:
-        #    if words in game_dict:
-
-   
-    #find_words(board, next_row, next_col, prefix, results )
-
-    # FIXME: one base case is that position row,col is not
-    #    available (could be off the board, could be currently
-    #    in use).  board.py can check that
-    # FIXME:  For the remaining cases, where the tile at row,col 
-    #    is available, we need to consider the new prefix that 
-    #    includes the letter on this tile
-    # FIXME:  Another base case is that no word can start with 
-    #    the current prefix.  No use searching further on that path.
-    # FIXME:  If the current position is a complete word, it is NOT 
-    #    a base case, because it might also be part of a longer word. 
-    #    We save the word we found into the global results list, and
-    #    continue with the recursive case. 
-    # FIXME: The recursive case is when the current prefix (including
-    #    the tile at row,col) is a possible prefix of a word.  We 
-    #    must mark it as currently in use, then search in all 8 directions
-    #    around it, and finally mark it as no longer in use. See board.py
-    #    for how to mark and unmark tiles, and how to get the text
-    #    on the current tile.     
-    return
-    
-
-def find_words(board, row, col, prefix, results):
-    """Find all words starting with prefix that
-    can be completed from row,col of board.
-    Args:
-        row:  row of position to continue from (need not be on board)
-        col:  col of position to continue from (need not be on board)
-        prefix: looking for words that start with this prefix
-        results: list of words found so far
-    Returns: nothing
-        (side effect is filling results list)
-    Effects:
-        inserts found words (not necessarily unique) into results
-    """
-
-    """Extracting content
-    for item in BoggleBoard.content:
-        row = BoggleBoard.content[item]
-        for column in row: 
-            col = row[column]
-    """
     print("At start of find_words, row col are: ", row, col)
+    try:
+        is_word
+    except NameError:
+        is_word = ""
 
     cur_tile = ""
-    if board.available(row, col) is False:
+    is_word = game_dict.search(row, col)
+    if board.available(row, col) is False or is_word == 0:
+        print("entered find_words with row_col marked taken and/or is_word is 0 so returning to main", board.available(row, col), is_word)
         return None
     else:
-        cur_tile = board.get_char(row,col)
+        cur_tile = board.get_char(row, col)
         board.mark_taken(row, col)
-        results.append(cur_tile)
+        #results.append(cur_tile)
+        if len(prefix) == 0:  # Only want to add current char to string if the prefix is empty.
+            prefix = cur_tile
 
     for i in range(-1, 2):
         for j in range(-1, 2):
             if board.available(row + i, col + j) is True:
                 print('get_char looking for location: ', row + i, col + j)
-                find_words(board, row + i, col + j, results, results) # need to fix args
+                cur_tile = board.get_char(row + i, col + j)
+                prefix += cur_tile
+                board.mark_taken(row + i , col + j)
+                is_word = game_dict.search(prefix)
+                print("value of is_word is *{}*.  prefix is *{}* and curr_char are: *{}*".format(is_word, prefix, cur_tile))
+                if is_word == 2:  # prefix is a prefix but not also a word
+                    board.unmark_taken(row + i , col + j) 
+                    find_words(board, row + i, col + j, prefix, results) # need to fix args
+                elif is_word == 1:  # prefix is a word
+                    results.append(prefix)
+                    board.unmark_taken(row + i , col + j)
+                    find_words(board, row + i, col + j, prefix, results)
+                else:  # prefix isn't in dict (is_word is 0)
+                    print("prefix isn't a word so returning to ?")
+                    break 
             else:
                 continue
-
+            break
+    print("Out of nexted for.  is_word is: ", is_word)
+    board.unmark_taken(row, col)
     print("Reults at end of nested for: ", results)
     return results
 
     
-def score(word):
-    """
-    Compute the Boggle score for a word, based on the scoring table
-    at http://en.wikipedia.org/wiki/Boggle. 
-    #FIXME: finish writing this docstring
-     """
-    #FIXME  score the word correctly
-    return 0
-
-
 
 ####
 # Run if invoked from command line
